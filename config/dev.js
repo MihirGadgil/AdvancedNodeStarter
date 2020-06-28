2bdca0e75e8b156b9794243a1b4444e20684b8ca
MihirGadgil <mihirgadgil95@gmail.com>
2020-06-28 17:43:08 +0530

    Added Cache

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://mihir1_rw:Iu6MUCa9h2PZyqyz@cluster0-shard-00-00-3vqdi.mongodb.net:27017,cluster0-shard-00-01-3vqdi.mongodb.net:27017,cluster0-shard-00-02-3vqdi.mongodb.net:27017/AdvStarter?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
