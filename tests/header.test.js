d37eed96fbc7822a29c3d9bd2e56dba338e3fc79
MihirGadgil <mihirgadgil95@gmail.com>
2020-09-07 22:56:24 +0530

    Adding More Testing

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+
 let browser, page;
 beforeEach(async () => {
     browser =  await puppeteer.launch({
@@ -23,5 +26,16 @@ test('clicking login starts OAuth flow', async () => {
     await page.click('.right a');
 
     const url = await page.url();
-    expect(url).toMatch(/account\.google\.com/);
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When Signin in, Logout Button shows', async () => {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+    await page.setCookie({ name: 'session', value: session });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]');
+    const logoutText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(logoutText).toBe('Logout');
 });
