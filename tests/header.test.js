faf17576d8c9290313456236024a5010b1f611d8
MihirGadgil <mihirgadgil95@gmail.com>
2020-09-08 22:35:26 +0530

    Completed implmentation for Custom Page

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,4 @@
 const Page = require('./helper/page');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const mongoose = require('mongoose');
 
 let page;
@@ -20,7 +18,6 @@ afterAll(async () => {
 test('Header must display Logo', async () => {
 
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-
     expect(text).toEqual('Blogster');
 });
 
@@ -32,12 +29,7 @@ test('clicking login starts OAuth flow', async () => {
 });
 
 test('When Signin in, Logout Button shows', async () => {
-    const user = await userFactory();
-    const { session, sig } = sessionFactory(user);
-    await page.setCookie({ name: 'session', value: session });
-    await page.setCookie({ name: 'session.sig', value: sig });
-    await page.goto('localhost:3000');
-    await page.waitFor('a[href="/auth/logout"]');
-    const logoutText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    await page.login();
+    const logoutText = await page.getContentOf('a[href="/auth/logout"]');
     expect(logoutText).toBe('Logout');
 });
