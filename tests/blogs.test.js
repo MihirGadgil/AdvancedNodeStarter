18a996312c834d9bca0132c8616f79a96c30e2df
MihirGadgil <mihirgadgil95@gmail.com>
2020-09-14 23:17:59 +0530

    added describe statements

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -15,9 +15,27 @@ afterAll(async () => {
     mongoose.disconnect();
 });
 
-test('When Logged in can see blog create form', async () => {
-    await page.login();
-    await page.click('a.btn-floating');
-    const label = await page.getContentOf('form label');
-    expect(label).toEqual('Blog Title');
+
+
+describe('When Logged in', async () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.click('a.btn-floating');
+    });
+    test('Can see blog create form', async () => {
+        const label = await page.getContentOf('form label');
+        expect(label).toEqual('Blog Title');
+    });
+    describe('And using Invalid Inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+        test('the form shows an error message', async() => {
+            const titleError = await page.getContentOf('.title .red-text');
+            const contentError = await page.getContentOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        });
+    });
 });
